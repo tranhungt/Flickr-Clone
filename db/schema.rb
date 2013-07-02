5d6b661d7f9e20792edce5206333ae5739734605
Hung Tran <tranhungt1@gmail.com>
2013-07-02 10:21:28 -0700

    gem install bootstrap, adds favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702161703) do
+ActiveRecord::Schema.define(:version => 20130702172033) do
+
+  create_table "favorites", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "photo_taggings", :force => true do |t|
     t.integer  "photo_id"
