9f0781d9ccca6ed3247725c6cc2ea6bcee80d8e1
Hung Tran <tranhungt1@gmail.com>
2013-07-04 09:35:58 -0700

    favorites page will instantly remove photo when a photo is unfavorited with AJAX

--- app/assets/javascripts/photo_favorite.js
+++
@@ -1,9 +0,0 @@
-
-$(function(){
-  $('.favorite_form').on('ajax:success', function(event){
-    console.log(this);
-    event.preventDefault();
-    $(event.currentTarget).toggleClass('hidden');
-    $(event.currentTarget).siblings().toggleClass('hidden');
-  });
-})
