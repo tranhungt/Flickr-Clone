9a8586fab5534e29f60027e61e4fb858093b160d
Hung Tran <tranhungt1@gmail.com>
2013-07-06 18:22:01 -0700

    updates production.rb config for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,8 @@ Flicker::Application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :s3_host_name => 's3-us-west-2.amazonaws.com'
+    :s3_host_name => 's3-us-west-1.amazonaws.com',
+
   }
 
 end
