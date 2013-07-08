9e48e71c2dd13ade6045b398a52236efc38d0a98
Hung Tran <tranhungt1@gmail.com>
2013-07-08 16:53:02 -0700

    display photostream's user's name/email

---
+++ app/assets/javascripts/ajax/search_autocomplete.js
@@ -0,0 +1,28 @@
+  $(function(){
+  $('#tag_search_input').autocomplete({
+                          source: getList(),
+                          minLength: 0,
+                          appendTo: '#custom-search-form',
+                          autoFocus: true
+  });
+
+  function getList(){
+    var titles = [];
+    var hash = {};
+    $.ajax({
+      type: "GET",
+      url: "/tags",
+      success: function(data){
+        var tags = $(data);
+        _(tags).each(function(tag){
+          titles.push(tag.title)
+          hash[tag.title] = tag.id;
+        })
+      }
+    })
+    console.log(titles)
+    return titles;
+
+  }
+  
+})
