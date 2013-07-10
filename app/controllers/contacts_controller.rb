f875c02914bb61a638e1befe5329f21f5287109b
Hung Tran <tranhungt1@gmail.com>
2013-07-10 14:04:13 -0700

    reorganizes contacts views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,17 @@ class ContactsController < ApplicationController
   before_filter :authenticate_user!
  def new
     @users = User.all
+    friends = current_user.friends
+    friends.each{|friend| @users.delete(friend)}
     @users.delete(current_user)
+
   end
 
   def list
     @friends = current_user.friends
   end
 
-  def recent_photosa
+  def recent_photos
     @photos = current_user.friend_photos
   end
 end
