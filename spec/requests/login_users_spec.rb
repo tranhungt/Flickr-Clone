68755c2798e6b4c549aa0735dbb4d7917939a548
Hung Tran <tranhungt1@gmail.com>
2013-11-01 11:42:06 -0700

    adds context to sign up and sign in specs

--- spec/requests/login_users_spec.rb
+++ spec/requests/login_users_spec.rb
@@ -3,14 +3,39 @@ require 'spec_helper'
 describe "LoginUsers" do
   before(:each) {visit new_user_session_path}
 
-  it "has email field" do 
-    page.should have_content("Email")
+  context "has appropriate login fields" do
+    it "has email field" do 
+      page.should have_content("Email")
+    end
+
+    it "has password field" do
+      page.should have_content("Password")
+    end
   end
 
-  it "has password field" do
-    page.should have_content("Password")
+  context "can direct to sign up page" do
+    it "should link to sign up page" do
+      click_link "Sign up"
+      page.should have_content("Sign up")
+    end
+
+    it "directs to sign up page when clicked" do
+      click_link "Sign up"
+      current_path.should == new_user_registration_path
+    end
   end
 
+  context "can direct to the forgot password page" do
+    it "has forgot password link" do
+      page.should have_content("Sign in")
+    end
+
+    it "directs to forgotten password page when link is clicked" do
+      click_link "Forgot your password?"
+      current_path.should == new_user_password_path
+    end
+  end
+  
   it "login user with valid credentials" do
     user = FactoryGirl.create(:user)
     fill_in "Email", :with => user.email
@@ -25,25 +50,6 @@ describe "LoginUsers" do
     fill_in "Password", :with => "wrong password"
     click_button "Sign in"
     current_path.should == new_user_session_path
-  end
-
-  it "should link to sign up page" do
-    click_link "Sign up"
-    page.should have_content("Sign up")
-  end
-
-  it "directs to sign up page when clicked" do
-    click_link "Sign up"
-    current_path.should == new_user_registration_path
-  end
-
-  it "has forgot password link" do
-    page.should have_content("Sign in")
-  end
-
-  it "directs to forgotten password page when link is clicked" do
-    click_link "Forgot your password?"
-    current_path.should == new_user_password_path
   end
 
 end
--- spec/requests/sign_up_user.rb
+++ spec/requests/sign_up_user.rb
@@ -3,36 +3,41 @@ require 'spec_helper'
 describe "SignUpUsers" do
   before(:each) {visit new_user_registration_path}
 
-  it "has link to sign in page" do
-    page.should have_content("Sign in")
+  context "can direct to sign in page" do
+    it "has link to sign in page" do
+      page.should have_content("Sign in")
+    end
+
+    it "directs to sign in page when link is clicked" do
+      click_link "Sign in"
+      current_path.should == new_user_session_path
+    end
   end
 
-  it "directs to sign in page when link is clicked" do
-    click_link "Sign in"
-    current_path.should == new_user_session_path
+  context "can direct to the forgot password page" do
+    it "has forgot password link" do
+      page.should have_content("Sign in")
+    end
+
+    it "directs to forgotten password page when link is clicked" do
+      click_link "Forgot your password?"
+      current_path.should == new_user_password_path
+    end
   end
 
-  it "has forgot password link" do
-    page.should have_content("Sign in")
-  end
+  context "has appropriate registration fields" do
+    it "has email field" do 
+      page.should have_content("Email")
+    end
 
-  it "directs to forgotten password page when link is clicked" do
-    click_link "Forgot your password?"
-    current_path.should == new_user_password_path
-  end
+    it "has password field" do
+      page.should have_content("Password")
+    end
 
-  it "has email field" do 
-    page.should have_content("Email")
+    it "has password confirmation field" do
+      page.should have_content("Password confirmation")
+    end
   end
-
-  it "has password field" do
-    page.should have_content("Password")
-  end
-
-  it "has password confirmation field" do
-    page.should have_content("Password confirmation")
-  end
-
   it "signs up with valid credentials" do
     email = "test@example.com"
     password = "password"
